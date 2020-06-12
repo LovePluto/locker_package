7546aeb062a49a53a19b5bc65ea7f6e4a373fad0
yuheng.wang <yuheng.wang@thoughtworks.com>
2020-06-12 19:32:01 +0800

    完成了task2

--- src/main/java/com/tdd/Locker.java
+++ src/main/java/com/tdd/Locker.java
@@ -1,7 +1,16 @@
 package com.tdd;
 
 public class Locker {
+    private int size;
+
+    public Locker(int size) {
+        this.size = size;
+    }
+
     public Ticket save(Pack pack) {
+        if (size == 0) {
+            return null;
+        }
         return new Ticket();
     }
 }
--- src/test/java/com/tdd/LockerTest.java
+++ src/test/java/com/tdd/LockerTest.java
@@ -7,12 +7,22 @@ public class LockerTest {
 
 
     @Test
-    public void should_get_ticket_when_given_locker_and_pack_then_save_pack(){
-        Locker locker = new Locker();
+    public void should_get_ticket_given_locker_and_pack_when_save_pack(){
+        Locker locker = new Locker(10);
         Pack pack = new Pack();
 
         Ticket ticket = locker.save(pack);
 
         assertNotNull(ticket);
+    }
+
+    @Test
+    public void should_get_null_given_locker_no_location_and_pack_when_save_pack(){
+        Locker locker = new Locker(0);
+        Pack pack = new Pack();
+
+        Ticket ticket = locker.save(pack);
+
+        assertNull(ticket);
     }
 }
